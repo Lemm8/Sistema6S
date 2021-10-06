99eaff00cf23605aa4ca4720f2e649d1a75f1ef8
Lemm8 <luis.meza.manriquez@hotmail.com>
2021-10-06 13:32:06 -0700

    Página de Inicio - versión 1.0

---
+++ sistema6s_frontend/sistema6s_frontend/src/layouts/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <nav class="navbar navbar-dark">
+            <a class="navbar-brand">Hutchinson</a>            
+        </nav>
+    </div>
+</template>
+
+
+
+<script>
+
+
+</script>
+
+
+
+<style>
+.navbar-brand {
+    margin-left: 5%;
+}
+
+.navbar {
+    background-color: #00008B;
+}
+</style>
