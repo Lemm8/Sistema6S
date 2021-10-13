531824dcd04d55bc9630ae1bf7e7f37e284367d5
Jorge <jorgeMG@jorgemdev.com>
2021-10-13 00:28:10 -0700

    Avance auditores, get request no funciona en vue.

--- sistema6s_frontend/sistema6s_frontend/src/App.vue
+++ sistema6s_frontend/sistema6s_frontend/src/App.vue
@@ -8,12 +8,18 @@
 <script>
 import Home from './views/Home.vue'
 import NavBar from './layouts/NavBar.vue'
-
+import axios from 'axios'
+window.axios = require('axios');
 export default {
   name: 'App',
   components: {
     Home,
     NavBar
+  },
+  mounted: function() {
+    axios.get("https://localhost:5001/api/auditores")
+         .then(response => console.log(response.data))
+         .catch(error => console.log(error)); 
   }
 }
 </script>
