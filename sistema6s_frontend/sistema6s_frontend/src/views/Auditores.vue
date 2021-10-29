13602d63bb3c61371ca41ab678ff1bbd4874de4b
Jorge <jorgeMG@jorgemdev.com>
2021-10-29 16:14:30 -0700

    Lista de auditores como lista usando Axios

---
+++ sistema6s_frontend/sistema6s_frontend/src/views/Auditores.vue
@@ -0,0 +1,59 @@
+
+
+<template>
+    <div class="container main">
+        <h2 id="title">Auditores</h2>
+        <div>
+            <ul v-for="auditor in info" :key="auditor.userId">
+                <li>
+                    {{auditor.nombre}}
+                </li>
+            </ul>
+        </div>
+    </div>    
+    
+</template>
+
+
+
+<script>
+import axios from 'axios' 
+window.axios = require('axios');
+//@btn-click="null"
+export default {
+    name: 'App',
+    data() {
+        return {
+            info : null
+        }
+    },
+    mounted: function() {
+    axios.get("https://localhost:5001/api/auditores")
+            .then(response => this.info = response.data)
+         .catch(error => console.log(error));
+
+  }
+}
+
+</script>
+
+
+
+<style>
+#title {
+    text-align: center;
+    margin-top: 30px;
+}
+
+.menu_div {
+    margin-top: 30px;
+}
+.row {
+    margin-top: 10px;
+}
+.button_div {
+    display: flex;
+    justify-content: center;
+}
+
+</style>
